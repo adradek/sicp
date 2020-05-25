f00c954bde8428e9572797ac42dd6ba9e40402f9
Alex Kochurov <alex.kochurov@gmail.com>
2020-05-25 17:05:18 +0300

    1.23 add cycle method for rust solution

--- 1.23/solution.rs
+++ 1.23/solution.rs
@@ -12,8 +12,9 @@ fn main() {
     ];
 
     let mut start = Instant::now();
-    let mut dur1 = start.elapsed();
-    let mut dur2 = start.elapsed();
+    let mut dur1: std::time::Duration;
+    let mut dur2: std::time::Duration;
+    let mut dur3: std::time::Duration;
 
     for num in numbers.iter() {
         start = Instant::now();
@@ -28,7 +29,13 @@ fn main() {
         }
         dur2 = start.elapsed() / 100;
 
-        println!("Prime {}: old took {:?}, new took {:?} ({} x faster)", num, dur1, dur2, dur1.as_secs_f64() / dur2.as_secs_f64());
+        start = Instant::now();
+        for _ in 0..100 {
+            rustish_way_find_divisor(*num);
+        }
+        dur3 = start.elapsed() / 100;
+
+        println!("Prime {}: old took {:?}, new took {:?}, rustish {:?} ({} x faster || {} x)", num, dur1, dur2, dur3, dur1.as_secs_f64() / dur2.as_secs_f64(), dur1.as_secs_f64() / dur3.as_secs_f64());
     }
 }
 
@@ -52,4 +59,24 @@ fn next_div(div: i64) -> i64 {
     } else {
         return div + 2;
     }
+}
+
+fn rustish_way_find_divisor(n: i64) -> i64 {
+    let mut div: i64 = 2;
+
+    loop {
+        if div * div > n {
+            break;
+        } else if n % div == 0 {
+            return div;
+        }
+
+        if div == 2 {
+            div = 3;
+        } else {
+            div = div + 2;
+        }
+    }
+
+    return n;
 }
